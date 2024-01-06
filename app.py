c2795b2c26aa6c18b93447c803207c8a51937ca4
hsnx999 <152194770+hsnx999@users.noreply.github.com>
2024-01-06 10:55:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 from torchvision import models, transforms
 import torch
 import streamlit as st
-from models import resnet
+from models
 
 # set title of app
 st.title("Simple Image Classification Application")
